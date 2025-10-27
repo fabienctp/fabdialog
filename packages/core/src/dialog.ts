e2d09362071c9d051ed971ad6283b9bfef76512c
[dyad] <git@dyad.sh>
2025-10-27 11:48:43 +0100

    Updated packages/core/src/dialog.ts

--- packages/core/src/dialog.ts
+++ packages/core/src/dialog.ts
@@ -8,7 +8,7 @@ export interface DialogOptions {
 }
 
 const MAXIMIZE_ICON_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-maximize-icon lucide-maximize"><path d="M8 3H5a2 2 0 0 0-2 2v3"/><path d="M21 8V5a2 2 0 0 0-2-2h-3"/><path d="M3 16v3a2 2 0 0 0 2 2h3"/><path d="M16 21h3a2 2 0 0 0 2-2v-3"/></svg>`;
-const MINIMIZE_ICON_SVG_FOR_EXPAND = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minimize"><path d="M8 3v3a2 0 0 1-2 2H3m18 0h-3a2 0 0 1-2-2V3m0 18v-3a2 0 0 1 2-2h3m-18 0h3a2 0 0 1 2 2v3"/></svg>`;
+const MINIMIZE_ICON_SVG_FOR_EXPAND = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minimize-icon lucide-minimize"><path d="M8 3v3a2 2 0 0 1-2 2H3"/><path d="M21 8h-3a2 2 0 0 1-2-2V3"/><path d="M3 16h3a2 2 0 0 1 2 2v3"/><path d="M16 21v-3a2 2 0 0 1 2-2h3"/></svg>`;
 
 
 export class Dialog {
