058ff63631dbc5785cd9688bb311e2c1055737f9
[dyad] <git@dyad.sh>
2025-10-27 12:00:54 +0100

    [dyad] Refactorisation des variables SCSS pour une gestion dynamique des couleurs de dialogue. - wrote 3 file(s), renamed 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./globals.css";
-import "./styles/vanilla-dialog.scss"; // Import the new SCSS file
+import "./styles/_globals.scss"; // Import the new SCSS file
+import "./styles/vanilla-dialog.scss"; // Import the new SCSS file for vanilla tabs
 import "./styles/_dialog-tabs.scss"; // Import the new SCSS file for vanilla tabs
 
 createRoot(document.getElementById("root")!).render(<App />);
