648cf486b3a0a73fca6766ed12d31aa97d64d111
[dyad] <git@dyad.sh>
2025-10-31 11:54:25 +0100

    [dyad] Correction des chemins d'import des styles SCSS. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/_globals.scss"; // Import the new SCSS file
-import "fab-dialog/src/styles/vanilla-dialog.scss"; // Import the new SCSS file for vanilla dialog
-import "fab-dialog/src/styles/_dialog-tabs.scss"; // Import the new SCSS file for vanilla tabs
+import "../packages/core/src/styles/vanilla-dialog.scss"; // Correction du chemin d'import
+import "../packages/core/src/styles/_dialog-tabs.scss"; // Correction du chemin d'import
 
 createRoot(document.getElementById("root")!).render(<App />);
